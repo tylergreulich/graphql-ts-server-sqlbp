f90d9ac7362b657cf6d87bfbd23cd2e75949a82f
tylergreulich <tgreul6@gmail.com>
2018-08-30 09:59:50 -0700

    Register Mutation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,10 +1,27 @@
 import { ResolverMap } from './types/graphql-utils';
+import { GQL } from './types/schema';
+import * as bcrypt from 'bcryptjs';
+import { User } from './entity/User';
 
 export const resolvers: ResolverMap = {
   Query: {
     hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || 'World'}`
   },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {}
+    registerUser: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        User.create({
+          email,
+          password: hashedPassword
+        }).save();
+        return true;
+      } catch (error) {
+        return error;
+      }
+    }
   }
 };
