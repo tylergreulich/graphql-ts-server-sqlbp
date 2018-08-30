095c83dfc69b90912730fbddb21c8368044ce19f
tylergreulich <tgreul6@gmail.com>
2018-08-30 09:22:26 -0700

    first commit

---
+++ src/entity/User.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  BeforeInsert,
+  BaseEntity
+} from 'typeorm';
+import * as uuidv4 from 'uuid/v4';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column('varchar', { length: 255 })
+  email: string;
+
+  // In PostgreSQL, 'text' allows it to be of any length.
+  // NOTE: You can't define the length yourself. Use 'varchar' for that.
+  @Column('text')
+  password: string;
+
+  @BeforeInsert()
+  addId() {
+    this.id = uuidv4();
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata';
+import { GraphQLServer } from 'graphql-yoga';
+import { importSchema } from 'graphql-import';
+import { resolvers } from './resolvers';
+
+const typeDefs = importSchema('schema.graphql');
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log(`Server running on localhost:4000`));
